e95adddfa97266e6fe8835912161de2bccce803b
maxingcong <maxingcong@matrixelements.com>
2021-11-29 14:26:49 +0800

    2.0版本开发

--- src/pages/userCenter/Profile.tsx
+++ src/pages/userCenter/Profile.tsx
@@ -1,9 +1,10 @@
 import { FC, useContext, useEffect, useState } from 'react'
-import { Form, Button, Input } from 'antd'
+import { Form, Button, Input, Upload, Image } from 'antd'
 import { useTranslation } from 'react-i18next'
 import { BaseInfoContext } from '@/layout/index'
-import { LoadingOutlined } from '@ant-design/icons'
+import { LoadingOutlined, PlusOutlined, MinusCircleFilled } from '@ant-design/icons'
 import './index.scss'
+import clean from '@assets/images/clean.icon.svg'
 
 const Profile: FC<any> = (props: any) => {
   const [form] = Form.useForm(),
@@ -11,9 +12,36 @@ const Profile: FC<any> = (props: any) => {
     baseInfo = useContext(BaseInfoContext),
     [Introduction, setIntroduction] = useState(''),
     [name, setName] = useState(''),
-    [head, setHead] = useState(''),
     [disabled, setDisabled] = useState(true),
-    [loading, setLoading] = useState(false)
+    [loading, setLoading] = useState(false),
+    [file, setFile] = useState<any>(''),
+    [fileBase, setFileBase] = useState<any>(''),
+    [TextAreaValue, setTextAreaValue] = useState(''),
+    { TextArea } = Input,
+    { Dragger } = Upload,
+    DraggerProps = {
+      name: 'file',
+      showUploadList: false,
+      accept: 'image/*',
+      beforeUpload: _ => false,
+      onChange(info) {
+        console.log(info.file);
+
+        setFile(info.file)
+        try {
+          const reader = new FileReader();
+          reader.readAsDataURL(info.file);
+          reader.onload = function () {
+            setFileBase(reader.result)
+          };
+        } catch (e) {
+          console.log(e, "错误处理");
+        }
+      },
+      onDrop(e) {
+        console.log('Dropped files', e.dataTransfer.files);
+      }
+    }
 
   useEffect(() => {
     form.setFieldsValue({
@@ -35,9 +63,12 @@ const Profile: FC<any> = (props: any) => {
         console.log(err);
 
       })
-  }
+  },
 
-
+    delFile = () => {
+      setFile('')
+      setFileBase('')
+    }
 
   return (<div className="layout-box">
     <div className="form-box userForm">
@@ -47,7 +78,7 @@ const Profile: FC<any> = (props: any) => {
         form={form}
         labelAlign="left"
         layout={'horizontal'}
-        wrapperCol={{ span: 14 }}
+        wrapperCol={{ span: 10 }}
         labelCol={{ span: i18n.language === 'en' ? 5 : 3 }}
       >
         <Form.Item colon label={t('UserCenter.ProfileOrganizationIdentifier')} name="identity"
@@ -64,14 +95,27 @@ const Profile: FC<any> = (props: any) => {
 
         <Form.Item colon label={t('UserCenter.ProfileOrganizationHead')} name="head"
           className="form-item">
-          <Input disabled={disabled} value={head} onChange={e => setHead(e.target.value)}
-            placeholder={t('UserCenter.ProfileHeadPlaceholder')} />
+          <Dragger
+            className="info-dragger"
+            disabled={!!file}
+            {...DraggerProps}
+          >
+            {!file ? <PlusOutlined /> : <Image src={fileBase} className="head-img" />}
+            {!file ? <p>{t("DidApplication.SetYourHeadIips")}</p> : <p className="head-img-lable">{file?.name || ''}</p>}
+          </Dragger>
+          {!!file ? <MinusCircleFilled onClick={delFile} className="del" /> : ''}
         </Form.Item>
 
         <Form.Item colon label={t('UserCenter.ProfileOrganizationIntroduction')} name="Introduction"
           className="form-item">
-          <Input disabled={disabled} value={Introduction} onChange={e => setIntroduction(e.target.value)}
-            placeholder={t('UserCenter.ProfileIntroductionPlaceholder')} />
+          {/* <Input disabled={disabled} value={Introduction} onChange={e => setIntroduction(e.target.value)}
+            placeholder={t('UserCenter.ProfileIntroductionPlaceholder')} /> */}
+          <TextArea autoSize={false} className="identfier-info-input"
+            value={TextAreaValue}
+            onChange={_ => setTextAreaValue(_.target.value)}
+            placeholder={t('UserCenter.ProfileIntroductionPlaceholder')} >
+          </TextArea>
+          {TextAreaValue ? <img src={clean} onClick={() => setTextAreaValue('')} className="clean" /> : ''}
         </Form.Item>
 
 
@@ -84,8 +128,7 @@ const Profile: FC<any> = (props: any) => {
                 {t('UserCenter.ModalCancel')}
               </Button>
               <Button type="primary" className="global-btn"
-                style={{ marginLeft: '30px' }}
-                icon={<LoadingOutlined />} loading={loading}
+                style={{ marginLeft: '30px' }} loading={loading}
                 onClick={() => submit()}>
                 {t('common.submit')}
               </Button>
