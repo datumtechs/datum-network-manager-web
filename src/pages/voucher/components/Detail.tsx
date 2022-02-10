a0793e479d12ed469dfc98b669fd43817d2adad1
maxingcong <maxingcong@matrixelements.com>
2022-02-10 11:30:14 +0800

    dev-0.4.0

--- src/pages/voucher/components/Detail.tsx
+++ src/pages/voucher/components/Detail.tsx
@@ -1,47 +1,79 @@
 import { FC, useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Button, Card } from 'antd'
+import { Button, Card, Tabs, Radio } from 'antd'
 import "../scss/styles.scss"
+const { TabPane } = Tabs;
 
 const Details: FC<any> = (props: any) => {
   const { t } = useTranslation()
+  const [trendSelect, setTrendSelect] = useState('1')
+
+  const priceCallback = () => { }
+  const credentialCallback = () => { }
+  const lineEchsrs = <div>123</div>
+  const tabs = (key, callback) => {
+    return <Tabs onChange={callback} key={key}>
+      <TabPane tab="Past 15D" key="1">
+        {lineEchsrs}
+      </TabPane>
+      <TabPane tab="Past 30D" key="2">
+        {lineEchsrs}
+      </TabPane>
+      <TabPane tab="Past 6M" key="3">
+        {lineEchsrs}
+      </TabPane>
+      <TabPane tab="Past 1Y" key="4">
+        {lineEchsrs}
+      </TabPane>
+      <TabPane tab="Past All" key="5">
+        {lineEchsrs}
+      </TabPane>
+    </Tabs>
+  }
 
   return <div>
     <Card className='details-top-box'>
       <div className='details-name-box'>
         <div className='address'>
-          <p>123</p>
-          <p>123</p>
+          <p>Credential name：XXXX名称（XX符号）</p>
+          <p>Contract address：XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</p>
         </div>
-        <Button>123</Button>
+        <Button>View on the block explorer</Button>
       </div>
       <div className='proce-box'>
         <div className='proce-left'>
-          <p></p>
-          <div></div>
+          <h4>Present price</h4>
+          <div className='price-card'>
+            <p>
+              1<sub>MTSTK-618</sub>
+              <span>=</span>
+              999<sub>LAT</sub>
+            </p>
+            <p>
+              1<sub>MTSTK-618</sub>
+              <span>=</span>
+              999<sub>LAT</sub>
+            </p>
+          </div>
         </div>
         <div className='proce-right'>
-          <div>
-            <Button></Button>
-            <Button></Button>
-          </div>
-          <div></div>
+          <Radio.Group
+            className='price-radio-btn'
+            value={trendSelect} onChange={e => setTrendSelect(e.target.value)} buttonStyle="solid">
+            <Radio.Button value="1">Price trend</Radio.Button>
+            <Radio.Button value="2">Volume trend</Radio.Button>
+          </Radio.Group>
+          {tabs('price', priceCallback)}
         </div>
       </div>
-
     </Card>
+    <Card className='details-bottom-box'>
+      <h4>Credential exchange pool</h4>
+      {tabs('credential', credentialCallback)}
+      <div className='credential-total'>
 
-
-
-
-
-
-
-
-
-
-
-
+      </div>
+    </Card>
   </div>
 }
 
