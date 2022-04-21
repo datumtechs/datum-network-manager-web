e40fe7b530274d040698b689e9f4b08a09831b41
maxingcong <41977768+maxingcong@users.noreply.github.com>
2022-04-21 11:15:22 +0800

    fix up dex bug

--- src/pages/voucher/components/PriceSet.tsx
+++ src/pages/voucher/components/PriceSet.tsx
@@ -19,57 +19,39 @@ const PriceSeting: FC<any> = (props: any) => {
     [routerToken, setRouterToken] = useState(''),
     [latValue, setLatValue] = useState(''),
     [mtsValue, setMstValue] = useState(''),
-    [spinning, setSpinning] = useState(false),
+    // [spinning, setSpinning] = useState(false),
     [submting, setSubmting] = useState(false),
     { walletConfig } = props.state,
     { location } = props,
     {
-      dataAddress,
-      name, dataTokenId, total, symbol } = location.state
+      dataAddress, name,
+      dataTokenId, total,
+      symbol
+    } = location.state
 
-  // const dataAddress = '0x38e5d728ccfa4be0849c249200820f6f68c13b0c',
-  // routerToken = '0xef5bad1b4bc03df3b6d62fe914e145126a5ff80d'
-  const fomatFloat = function (value, n) {
-    var f = Math.round(value * Math.pow(10, n)) / Math.pow(10, n);
-    var s = f.toString();
-    return s;
-  }
 
-  const rate = () => {
-    if (!!!mtsValue || !!!latValue) return ''
-    // debugger
-    let value: string | number = parseFloat(new Big(BigInt(mtsValue)).div(BigInt(latValue)))
-    value = fomatFloat(value, 8)
+  const divFn = (type?) => {
+    if (BigInt(mtsValue) <= BigInt(0) || BigInt(latValue) <= BigInt(0)) return ''
+    let value: string = type ? new Big(mtsValue).div(latValue).toFixed(8) : new Big(latValue).div(mtsValue).toFixed(8)
+    value = value.replace(/(?:.0*|(.\d+?)0+)$/, '$1')
     return value
   }
 
-  const divFn = () => {
-    if (!!!mtsValue || !!!latValue) return ''
-    let value: string | number = parseFloat(new Big(BigInt(latValue)).div(BigInt(mtsValue)))
-    value = fomatFloat(value, 8)
-    return value
-  }
-
-  //数据授权 授权
   const toAuthorization = async (web3, address) => {
-
-
     const contract = new web3.eth.Contract(      //构建 数据 合约 
       ERC20,
       dataAddress
     );
-
-
     const amound = await contract.methods.allowance(
       address,
       routerToken
     ).call()
-    // console.log(amound)
+    console.log(amound)
     if (amound > 0) return
 
     await contract.methods.approve( //数据凭证授权
-      routerToken,//像合约数据凭证授权
-      total,
+      routerToken,
+      total
     ).send({ from: address })
   }
 
@@ -80,9 +62,6 @@ const PriceSeting: FC<any> = (props: any) => {
       const { web3 } = wallet
 
       // 1 获取地址
-      // const flag = await wallet.eth.isConnected()//判断是否连接当前网络
-      // if (!flag) return
-
       const address = await wallet.connectWallet(walletConfig)
       if (!address) {
         return message.warning(t('common.pleaseSwitchNetworks'))
@@ -92,13 +71,11 @@ const PriceSeting: FC<any> = (props: any) => {
       if (BigInt(balance) < BigInt(latValue + Complement)) {
         return message.warning(t('common.currentWalletInsufficient'))
       }
-      // return
 
       //数据授权
-      setSpinning(true)
+      // setSpinning(true)
       setSubmting(true)
       await toAuthorization(web3, address[0])
-
       //构建合约
       const myContract = new web3.eth.Contract(
         ABIJson,
@@ -108,7 +85,7 @@ const PriceSeting: FC<any> = (props: any) => {
       //获取nonce
       const nonce = await web3.eth.getTransactionCount(address[0])
 
-      setSpinning(false)
+      // setSpinning(false)
       //ERC20 合约的方法签名
       /**
        * function addLiquidityETH(
@@ -121,7 +98,7 @@ const PriceSeting: FC<any> = (props: any) => {
        * ) external payable returns (uint amountToken, uint amountETH, uint liquidity);
        */
       //发起交易
-      console.log(mtsValue + Complement)
+
       const contract = await myContract.methods.addLiquidityETH(
         dataAddress,
         mtsValue + Complement,// 兑换的值
@@ -131,13 +108,11 @@ const PriceSeting: FC<any> = (props: any) => {
         Date.now() + 1200000 //区块时间+20分钟
       )
 
-
       const gas = await contract.estimateGas({
         from: address[0],
         value: latValue + Complement,
-      })
+      }).catch(err => console.log(err))
       const gasPrice = await web3.eth.getGasPrice()
-
       await contract.send({
         from: address[0],
         value: latValue + Complement,//let
@@ -148,7 +123,7 @@ const PriceSeting: FC<any> = (props: any) => {
       })
     } catch (e) {
       setSubmting(false)
-      setSpinning(false)
+      // setSpinning(false)
       console.log('发起交易失败', e)
       message.warning(t('tip.operationFailed'))
     }
@@ -157,7 +132,6 @@ const PriceSeting: FC<any> = (props: any) => {
 
   const submit = async () => {
     const totalNum = total.replace(Complement, '')
-    // console.log(totalNum)
     if (BigInt(totalNum) < BigInt(mtsValue)) {
       message.warning(t('voucher.hasExceeded'))
       return
@@ -185,7 +159,6 @@ const PriceSeting: FC<any> = (props: any) => {
       pathname: '/voucher/NoAttribute',
       state: {
         attributeType: type == 'type' ? 'Un' : '',
-        // attributeType: 'Unpriced'
       },
     })
   }
@@ -196,63 +169,62 @@ const PriceSeting: FC<any> = (props: any) => {
       const { data } = res
       if (data.routerToken) {
         setRouterToken(data.routerToken)
-        // setRouterToken('0x26D637E206Cc39942628421e7B0D6Fb41dB0bC06')
       }
     })
   }, [])
 
   return <div className='price-seting'>
-    <Spin wrapperClassName="wrap-spin" tip={t('common.dataAuthProgress')} spinning={spinning}>
-      <Card className='details-top-box layout-box'>
-        <div className='details-name-box'>
-          <div className='address'>
-            <p>{t('voucher.VoucherName')}：{name}</p>
-            <p>{t('voucher.ContractAddress')}：{dataAddress}</p>
+    {/* <Spin wrapperClassName="wrap-spin" tip={t('common.dataAuthProgress')} spinning={spinning}> */}
+    <Card className='details-top-box layout-box'>
+      <div className='details-name-box'>
+        <div className='address'>
+          <p>{t('voucher.VoucherName')}：{name}</p>
+          <p>{t('voucher.ContractAddress')}：{dataAddress}</p>
+        </div>
+      </div>
+      <div className='speed-progress'>
+        <img src={stepTwo} alt="" />
+      </div>
+      <p className='price-exchange-title'>{t('voucher.PleaseSetPriced')}：</p>
+      <div className='price-exchange'>
+        <div className='price-lat'>
+          <p className='price-type-title'>Lat</p>
+          <div className='price-type-input'>
+            <span>{t('voucher.Add')}</span>
+            <Input value={latValue} onChange={v => setLatValue(String(v.target.value).replace(/\D/g, ''))} maxLength={18} />
           </div>
         </div>
-        <div className='speed-progress'>
-          <img src={stepTwo} alt="" />
-        </div>
-        <p className='price-exchange-title'>{t('voucher.PleaseSetPriced')}：</p>
-        <div className='price-exchange'>
-          <div className='price-lat'>
-            <p className='price-type-title'>Lat</p>
-            <div className='price-type-input'>
-              <span>{t('voucher.Add')}</span>
-              <Input value={latValue} onChange={v => setLatValue(String(v.target.value).replace(/\D/g, ''))} maxLength={18} />
-            </div>
-          </div>
 
-          <div className='price-icon'>
-            <img src={exchange} alt="" />
-          </div>
-          <div className='price-mtstk'>
-            <Tooltip placement="top" title={symbol}>
-              <p className='price-type-title'>{symbol}</p>
-            </Tooltip>
-            <p className='price-secondary-title'>{t('voucher.Circulation')}：{total && total.replace(Complement, '') || ''}</p>
-            <div className='price-type-input'>
-              <span>{t('voucher.Add')}</span>
-              <Input value={mtsValue} onChange={v => setMstValue(String(v.target.value).replace(/\D/g, ''))} maxLength={18} />
-            </div>
-          </div>
+        <div className='price-icon'>
+          <img src={exchange} alt="" />
         </div>
-        <div className='price-exchange-tips'>
-          <img src={warning} alt="" />
-          <div>
-            <p>{t('voucher.InitialPrice')}：{mtsValue && mtsValue ? 1 : 0} {symbol} = {divFn() || 0} LAT ； {latValue && mtsValue ? 1 : 0} LAT = {rate() || 0} {symbol} </p>
-            <p>{t('voucher.InitialPriceTips')}.</p>
+        <div className='price-mtstk'>
+          <Tooltip placement="top" title={symbol}>
+            <p className='price-type-title'>{symbol}</p>
+          </Tooltip>
+          <p className='price-secondary-title'>{t('voucher.Circulation')}：{total && total.replace(Complement, '') || ''}</p>
+          <div className='price-type-input'>
+            <span>{t('voucher.Add')}</span>
+            <Input value={mtsValue} onChange={v => setMstValue(String(v.target.value).replace(/\D/g, ''))} maxLength={18} />
           </div>
         </div>
-        <div className='exchange-button'>
-          <Button className='but' onClick={goNoAttribute}>{t('common.return')}</Button>
-          <Button type="primary" className="but _submit"
-            disabled={!(latValue && mtsValue)}
-            loading={submting}
-            onClick={submit}>{t('voucher.Confirm')}</Button>
+      </div>
+      <div className='price-exchange-tips'>
+        <img src={warning} alt="" />
+        <div>
+          <p>{t('voucher.InitialPrice')}：{mtsValue && mtsValue ? 1 : 0} {symbol} = {divFn() || 0} LAT ； {latValue && mtsValue ? 1 : 0} LAT = {divFn('rate') || 0} {symbol} </p>
+          <p>{t('voucher.InitialPriceTips')}.</p>
         </div>
-      </Card>
-    </Spin>
+      </div>
+      <div className='exchange-button'>
+        <Button className='but' onClick={goNoAttribute}>{t('common.return')}</Button>
+        <Button type="primary" className="but _submit"
+          disabled={!(latValue && mtsValue)}
+          loading={submting}
+          onClick={submit}>{t('voucher.Confirm')}</Button>
+      </div>
+    </Card>
+    {/* </Spin> */}
   </div>
 }
 
